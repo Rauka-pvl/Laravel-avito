114caf5b655d1a553265c927856204b5297b26d0
Рауат Аделканов <rauatadelkanov@MacBook-Air-Rauat.local>
2025-01-30 13:19:49 +0500

    add getOnArticul and Brand cors

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         "api/imagesM/storeM",
-        '/multifinderbrands.php',
+        'multifinderbrands.php',
     ];
 }
