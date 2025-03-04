ec4896c5a8cf446b877946c26000f03b00843c21
Rauka-pvl <mr.redle3@gmail.com>
2025-03-04 09:40:47 +0500

    edit UpdateController find problem

--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -35,16 +35,19 @@ class UpdateController extends Controller
         exec($command);
 
         // PHP-код продолжает выполняться сразу после запуска Python-скрипта
-        // return redirect()->back()->with(['success' => 'Запуск обновления цен и фотографий запущен']);
+        return redirect()->back()->with(['success' => 'Запуск обновления цен и фотографий запущен']);
     }
     public function updateTrast()
     {
         $pythonScript = '/home/admin/web/233204.fornex.cloud/public_html/python_modules/price_photo_update/multi_parser.py';
 
         // Формируем команду для запуска в фоне
-        $command = "nohup python3 $pythonScript > /dev/null 2>&1 &";
+        // $command = "nohup python3 $pythonScript > /dev/null 2>&1 &";
 
         // Выполняем команду
+        // exec($command);
+
+        $command = "nohup python3 /home/admin/web/233204.fornex.cloud/public_html/python_modules/price_photo_update/multi_parser.py > /home/admin/logs/multi_parser.log 2>&1 &";
         exec($command);
 
         // PHP-код продолжает выполняться сразу после запуска Python-скрипта
