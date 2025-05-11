e8af7d59aab4c6de2a517eecf1b0806bdf619053
vinte97 <yura-pvl@mail.ru>
2025-05-11 08:32:33 +0500

    utf8 fix

--- python_modules/trast/main.py
+++ python_modules/trast/main.py
@@ -38,7 +38,9 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
-
+with open(log_filename, "w", encoding="utf-8-sig") as f:
+    f.write("")  # Просто создаст файл с BOM
+logging.FileHandler(log_filename, encoding="utf-8-sig")
 logger = logging.getLogger(__name__)
 
 # === Парсинг и Excel-функции ===
