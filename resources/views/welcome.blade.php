6d56a5978a5788682c127e08c93a7420be93fd49
Rauka-pvl <mr.redle3@gmail.com>
2025-06-12 00:33:06 +0500

    store multiple

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -419,6 +419,21 @@
                     </g>
                 </svg>
             </div>
+            <div>
+                @if (Route::has('login'))
+                    @auth
+                        <a href="{{ url('/dashboard') }}"
+                            class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}"
+                                class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        @endif
+                    @endauth
+                @endif
+            </div>
         </div>
     </div>
 </body>
