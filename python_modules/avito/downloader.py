7cbc0b7a226847d0544beb0a88461a1193f618ce
vinte97 <yura-pvl@mail.ru>
2025-06-23 14:19:39 +0500

    download fix

--- python_modules/avito/downloader.py
+++ python_modules/avito/downloader.py
@@ -23,8 +23,17 @@ def download_if_changed(url):
 
 def download_all():
     updated_files = []
+
     for url in XML_URLS:
         file = download_if_changed(url)
         if file:
             updated_files.append(file)
+
+    # Если нет новых файлов — используем все старые из кэша
+    if not updated_files:
+        for url in XML_URLS:
+            filename = os.path.join(CACHE_DIR, url_to_filename(url))
+            if os.path.exists(filename):
+                updated_files.append(filename)
+
     return updated_files
